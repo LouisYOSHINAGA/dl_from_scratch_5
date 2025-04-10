957f7c9a30d603d3a483f1ca64e1a5a5c91def34
Louis YOSHINAGA <9dutat.e@gmail.com>
2025-04-10 23:52:22 +0900

    Add: norm_param.py

---
+++ step01/norm_param.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def normal(x: float|np.ndarray, mu: float =0.0, sigma: float =1.0) -> float|np.ndarray:
+    return 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / (2 * sigma**2))
+
+
+if __name__ == "__main__":
+    x: np.ndarray = np.linspace(-10, 10, 1000)
+
+
+    y0: np.ndarray = normal(x, mu=-3)
+    y1: np.ndarray = normal(x, mu=0)
+    y2: np.ndarray = normal(x, mu=5)
+
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(x, y0, label="$\\mu$=-3")
+    plt.plot(x, y1, label="$\\mu$=0")
+    plt.plot(x, y2, label="$\\mu$=5")
+    plt.legend()
+    plt.show()
+
+
+    y0: np.ndarray = normal(x, sigma=0.5)
+    y1: np.ndarray = normal(x, sigma=1)
+    y2: np.ndarray = normal(x, sigma=2)
+
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(x, y0, label="$\\sigma$=0.5")
+    plt.plot(x, y1, label="$\\sigma$=1")
+    plt.plot(x, y2, label="$\\sigma$=2")
+    plt.legend()
+    plt.show()
