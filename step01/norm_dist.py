73c25f5c23039b87131c5825a2a235ef869e1353
Louis YOSHINAGA <9dutat.e@gmail.com>
2025-04-11 01:46:02 +0900

    Fix: typing

--- step01/norm_dist.py
+++ step01/norm_dist.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def normal(x: float|np.ndarray, mu: float =0.0, sigma: float =1.0) -> float|np.ndarray:
+
+def normal(x: np.ndarray, mu: float =0.0, sigma: float =1.0) -> np.ndarray:
     return 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / (2 * sigma**2))
+
 
 if __name__ == "__main__":
     x: np.ndarray = np.linspace(-5, 5, 100)
     y: np.ndarray = normal(x)
 
+    # fig. 1-5
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(x, y)
--- step01/norm_param.py
+++ step01/norm_param.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def normal(x: float|np.ndarray, mu: float =0.0, sigma: float =1.0) -> float|np.ndarray:
+def normal(x: np.ndarray, mu: float =0.0, sigma: float =1.0) -> np.ndarray:
     return 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / (2 * sigma**2))
 
 
 if __name__ == "__main__":
     x: np.ndarray = np.linspace(-10, 10, 1000)
 
-
     y0: np.ndarray = normal(x, mu=-3)
     y1: np.ndarray = normal(x, mu=0)
     y2: np.ndarray = normal(x, mu=5)
 
+    # fig. 1-6
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(x, y0, label="$\\mu$=-3")
@@ -27,6 +27,7 @@ if __name__ == "__main__":
     y1: np.ndarray = normal(x, sigma=1)
     y2: np.ndarray = normal(x, sigma=2)
 
+    # fig. 1-7
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(x, y0, label="$\\sigma$=0.5")
