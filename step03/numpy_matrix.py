fdb094bdc626506226f7d1da7bfda1ecaa6153b6
Louis YOSHINAGA <9dutat.e@gmail.com>
2025-04-17 23:58:48 +0900

    Fix: numpy_matrix.py

--- step03/numpy_matrix.py
+++ step03/numpy_matrix.py
@@ -11,16 +11,16 @@ print(f"{d=}")
 print(f"{B=}")
 print(f"{A @ B =}")
 
-def multivariate_normal(x: np.ndarray, mu: np.ndarray, cov: np.ndarray) -> np.ndarray:
-    det: float = np.linalg.det(cov)
+def multivariate_normal(x: np.ndarray, mu: np.ndarray, cov: np.ndarray) -> np.float64:
+    det: np.float64 = np.linalg.det(cov)
     inv: np.ndarray = np.linalg.inv(cov)
     D: int = len(x)
-    z = 1 / np.sqrt((2 * np.pi) ** D * det)
-    y = z * np.exp(- (x - mu).T @ inv @ (x - mu) / 2.0)
+    z: np.float64 = 1 / np.sqrt((2 * np.pi) ** D * det)
+    y: np.float64 = z * np.exp(- (x - mu).T @ inv @ (x - mu) / 2.0)  # type: ignore
     return y
 
-x: np.ndarray = np.array([[0], [0]])
-mu: np.ndarray = np.array([[1], [2]])
+x: np.ndarray = np.array([0, 0])
+mu: np.ndarray = np.array([1, 2])
 cov: np.ndarray = np.array([[1, 0], [0, 1]])
-y: np.ndarray = multivariate_normal(x, mu, cov)
+y: np.float64 = multivariate_normal(x, mu, cov)
 print(f"{y=}")
